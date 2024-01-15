566184c1e3becee05d5cd9113f438814521ba6b5
HyunWoo4biglab <57884870+HyunWoo4biglab@users.noreply.github.com>
2024-01-15 17:52:00 +0900

    Add files via upload

--- roi_ref_run_dir.sh
+++ roi_ref_run_dir.sh
@@ -27,13 +27,18 @@ fasta_suff=".roi_ref_kmer.fasta"
 for family_id in "$target_dir"/*; do
     target_family="$target_dir/$family_id/"
     for target_sample in "$target_family"/*; do
-        input_sample="$target_family/$target_sample"
-        if [! -f "$input_file"]; then
-            echo "Error: Input file not found.!"
-            continue
-        fi
+        input_dir="$target_family/$target_sample/"
+        #if [! -f "$input_file"]; then
+        #    echo "Error: Input file not found.!"
+        #    continue
+        #fi
 
         # Run the code for the current sample
-        python get_roi_reference_kmer.py -v "$input_sample$vcf_suff" -g "$genome" -f "$input_sample$fasta_suff" -k "$input_sample.sample" -o "$output_dir/$target_sample" -p "$sample_id.roi_ref_kmer.pickle" -c 200
+        python get_roi_reference_kmer.py -v "$input_dir$target_sample$vcf_suff" -g "$genome" -f "$input_dir$target_sample$fasta_suff" -k "$input_dir$target_sample.sample" -o "$input_dir" -p "$input_dir$target_sample.roi_ref_kmer.pickle" -c 200
+        kmc -v -fa -k31 -ci1 -t10 -m8 "$input_dir$target_sample$fasta_suff" "$input_dir$target_sample.roi_ref_kmer" "input_dir"
+        kmc_tools simple "$input_dir$target_sample.sample" "$input_dr$target_sample.roi_ref_kmer" intersect "$input_dir$target_sample.roi_ref_kmer_count" -ocleft
+        kmc_tools transform "$input_dir$target_sample.roi_ref_kmer_count" dump "$input_dir$target_sample.roi_ref_kmer_count_dump.txt"
+        python save_kmerdb_pickle.py -i "$input_dir$target_sample.roi_ref_kmer_count_dump.txt" -p "$input_dir$target_sample.roi_ref_kmer_count.pickle"
+        echo "kmer db pickle object saved at $input_dir$target_sample.roi_ref_kmer_count.pickle"
 
 done
